935c482b305031231115a7a3e3a38e1f8aada3d9
Hasan Alperen <78741582+alperen-selcuk@users.noreply.github.com>
2021-10-25 18:35:36 +0300

    Create install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#ubuntu üzerine docker ve docker-compose kurmak için gerekli script.
+
+sudo apt-get update
+
+sudo apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce
+
+sudo usermod -aG docker $USER
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
