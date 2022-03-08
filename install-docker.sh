8510efd093f765e3333a1876257a829b5fc9fd88
Hasan Alperen <78741582+alperen-selcuk@users.noreply.github.com>
2022-03-08 17:51:33 +0300

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -26,3 +26,7 @@ sudo usermod -aG docker $USER
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
+
+sudo systemctl start docker
+
+sudo systemctl enable docker
