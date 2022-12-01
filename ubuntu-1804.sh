9771032dd6924e712077ae55c4f08a853b1e0874
Hasan Alperen <78741582+alperen-selcuk@users.noreply.github.com>
2022-12-01 22:59:52 +0300

    update script

--- ubuntu-1804.sh
+++ ubuntu-1804.sh
@@ -8,18 +8,19 @@ sudo apt install -y \
     apt-transport-https \
     ca-certificates \
     curl \
-    software-properties-common
+    software-properties-common \
+    gnupg \
+    lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
+sudo apt update
 
-sudo apt-get install -y docker-ce
+sudo apt install docker-ce docker-ce-cli containerd.io
 
 sudo usermod -aG docker $USER
 
