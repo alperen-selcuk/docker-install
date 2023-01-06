5a563575c541d1abd0a5d44e663d297db8abcf25
Hasan Alperen <78741582+alperen-selcuk@users.noreply.github.com>
2023-01-06 23:20:20 +0300

    Update ubuntu-1804.sh

--- ubuntu-1804.sh
+++ ubuntu-1804.sh
@@ -31,8 +31,12 @@ sudo systemctl start docker
 
 sudo systemctl enable docker
 
-sleep 3
+printf '\nDocker installed successfully\n\n'
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | sort --version-sort | tail -n 1`
+
+sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
 
 sudo chmod +x /usr/local/bin/docker-compose
+
+printf '\nDocker Compose installed successfully\n\n'
