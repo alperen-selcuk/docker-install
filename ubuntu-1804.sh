d10b4ab1b19cd49d4b9778c1f89e83eec6d308ba
Hasan Alperen <78741582+alperen-selcuk@users.noreply.github.com>
2023-01-07 23:02:39 +0300

    Update ubuntu-1804.sh

--- ubuntu-1804.sh
+++ ubuntu-1804.sh
@@ -20,7 +20,7 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 
 sudo apt update
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 sudo usermod -aG docker $USER
 
