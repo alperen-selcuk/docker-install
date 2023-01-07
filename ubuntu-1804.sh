a5748a32e69cf9dc5985d08e1a8a7873826719c7
Hasan Alperen <78741582+alperen-selcuk@users.noreply.github.com>
2023-01-07 23:36:24 +0300

    Update ubuntu-1804.sh

--- ubuntu-1804.sh
+++ ubuntu-1804.sh
@@ -10,7 +10,9 @@ sudo apt install -y \
     curl \
     software-properties-common \
     gnupg \
-    lsb-release
+    lsb-release \
+    gnupg2 \
+    pass
 
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
