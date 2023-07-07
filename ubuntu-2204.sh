59a1f7e200752defd0170d4f2aebf856187ed486
Hasan Alperen SELÇUK <78741582+alperen-selcuk@users.noreply.github.com>
2023-07-07 15:34:30 +0300

    Update ubuntu-2204.sh

--- ubuntu-2204.sh
+++ ubuntu-2204.sh
@@ -4,19 +4,24 @@ sudo apt update
 
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
 sudo apt update
 
-apt-cache policy docker-ce
+#apt-cache policy docker-ce
+apt-cache madison docker-io 
 
-sudo apt install docker-ce
+#sudo apt install docker-ce
+sudo apt install docker-io
+
+sudo groupadd docker
+sudo usermod -aG docker ${USER}  
 
 sudo systemctl enable docker && sudo systemctl start docker
-
-sudo usermod -aG docker ${USER}
 
 mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
