6b32d3ac449659fb3621fcab89fa4111b64d0714
Hasan Alperen SELÇUK <78741582+alperen-selcuk@users.noreply.github.com>
2023-11-21 00:29:07 +0300

    Create ubuntu_2204.sh

---
+++ ubuntu_2204.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Docker Kurulumu
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+# Docker-Compose Kurulumu
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Docker kullanıcısını ekleyerek sudo gerekmeden kullanabilme
+sudo groupadd docker
+sudo usermod -aG docker $USER
