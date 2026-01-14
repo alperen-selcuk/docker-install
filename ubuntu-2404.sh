282886800748feeb6d8ded09b94b22421e627acd
Hasan Alperen SELÇUK <78741582+alperen-selcuk@users.noreply.github.com>
2026-01-14 19:04:18 +0300

    Update ubuntu-2404.sh

--- ubuntu-2404.sh
+++ ubuntu-2404.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 58118A87DAD84939
+sudo apt install -y ca-certificates curl
 
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
 
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/linux-x86_64" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 sudo systemctl start docker
+sudo systemctl enable docker
 sudo systemctl status docker
 
+sudo usermod -aG docker $USER
 
